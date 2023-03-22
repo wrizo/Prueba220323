6ff9e248f941d0db2fbd67c18dec10d00e96f42b
Melvinrl <melvinrl@hotmail.com>
2023-03-22 17:50:16 -0600

    Clase para calcular el rectanguo

---
+++ Main/src/CalculoFigurasGeometricas/Rectangulo.java
@@ -0,0 +1,12 @@
+package CalculoFigurasGeometricas;
+
+public class Rectangulo {
+
+    public double resultado;
+
+    public double areaRectangulo(double base, double altura){
+
+        resultado=(base*altura)/2;
+        return resultado;
+    }
+}
