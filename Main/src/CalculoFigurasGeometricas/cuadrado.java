870faa6bd4ab264b7f7c5559d402d6a0f5d45ade
arsu28 <arsu28@gmail.com>
2023-03-22 17:51:40 -0600

    Calculando area cuadrado

---
+++ Main/src/CalculoFigurasGeometricas/cuadrado.java
@@ -0,0 +1,8 @@
+package CalculoFigurasGeometricas;
+
+public class cuadrado {
+    public double calcular_area_cuadrado(double lado){
+         double area = lado * lado;
+         return area;
+    }
+}
